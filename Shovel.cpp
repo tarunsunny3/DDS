e141d51759057368a865fe8f9d52fe3994d38490
Tarun Chinthakindi <tarunsunny3@gmail.com>
2023-06-29 07:59:34 +0000

    corrected TonnageListener virtual function error

--- Shovel.cpp
+++ Shovel.cpp
@@ -20,6 +20,33 @@
 #include <ace/OS_NS_time.h>
 class TonnageListener : public DDS::DataReaderListener {
 public:
+
+  explicit TonnageListener() {}
+
+  virtual void on_requested_deadline_missed(
+    DDS::DataReader_ptr,
+    const DDS::RequestedDeadlineMissedStatus&) {}
+
+  virtual void on_requested_incompatible_qos(
+    DDS::DataReader_ptr,
+    const DDS::RequestedIncompatibleQosStatus&) {}
+
+  virtual void on_sample_rejected(
+    DDS::DataReader_ptr,
+    const DDS::SampleRejectedStatus&) {}
+
+  virtual void on_liveliness_changed(
+    DDS::DataReader_ptr,
+    const DDS::LivelinessChangedStatus&) {}
+
+  virtual void on_subscription_matched(
+    DDS::DataReader_ptr,
+    const DDS::SubscriptionMatchedStatus&) {}
+
+  virtual void on_sample_lost(
+    DDS::DataReader_ptr,
+    const DDS::SampleLostStatus&) {}
+
   virtual void on_data_available(DDS::DataReader_ptr reader) {
     Mining::TonnageDataReader_var reader_i =
       Mining::TonnageDataReader::_narrow(reader);
